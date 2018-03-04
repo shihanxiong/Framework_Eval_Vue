38e9d91d0251995bb481d011dbb27223c84fcd13
shihanxiong <shihanxiong@gmail.com>
2018-03-04 17:55:27 -0500

    update

--- ComponentCommunication/src/components/User.vue
+++ ComponentCommunication/src/components/User.vue
@@ -2,32 +2,49 @@
     <div class="component">
         <h1>The User Component</h1>
         <p>I'm an awesome User!</p>
+        <button @click="changeName">Change My Name</button>
+        <p>Name is {{ name }}</p>
+        <p>Age is {{ age }}</p>
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail></app-user-detail>
+                <app-user-detail :myName="name" :userAge="age" @nameWasReset="name = $event" :resetFn="resetName"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
-                <app-user-edit></app-user-edit>
+                <app-user-edit :userAge="age" @ageWasEdited="age = $event"></app-user-edit>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import UserDetail from './UserDetail.vue';
-    import UserEdit from './UserEdit.vue';
+import UserDetail from "./UserDetail.vue";
+import UserEdit from "./UserEdit.vue";
 
-    export default {
-        components: {
-            appUserDetail: UserDetail,
-            appUserEdit: UserEdit
-        }
+export default {
+  data: function() {
+    return {
+      name: "Max",
+      age: 27
+    };
+  },
+  methods: {
+    changeName() {
+      this.name = "Anna";
+    },
+    resetName() {
+      this.name = "Max";
     }
+  },
+  components: {
+    appUserDetail: UserDetail,
+    appUserEdit: UserEdit
+  }
+};
 </script>
 
 <style scoped>
-    div {
-        background-color: lightblue;
-    }
+div {
+  background-color: lightblue;
+}
 </style>
