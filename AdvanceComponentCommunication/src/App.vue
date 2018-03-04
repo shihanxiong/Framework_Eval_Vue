17432fed6fcac3a5648d62f1d65f7cd4e2641362
shihanxiong <shihanxiong@gmail.com>
2018-03-04 18:04:48 -0500

    update

--- AdvanceComponentCommunication/src/App.vue
+++ AdvanceComponentCommunication/src/App.vue
@@ -1,19 +1,23 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-               
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <appQuote quote="A wonderful quote!"></appQuote>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
+import Quote from "./components/Quote.vue";
 
-    export default {
-        
-    }
+export default {
+  components: {
+    appQuote: Quote
+  }
+};
 </script>
 
 <style>
+
 </style>
