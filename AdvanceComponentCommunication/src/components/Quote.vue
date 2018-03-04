2c49355ed98058ff398c5753a34b6cdc198d4dd4
shihanxiong <shihanxiong@gmail.com>
2018-03-04 18:16:08 -0500

    using slots

--- AdvanceComponentCommunication/src/components/Quote.vue
+++ AdvanceComponentCommunication/src/components/Quote.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <slot></slot>
+    <div class="title">
+      <slot name="title"></slot>
+    </div>
+    <hr>
+    <div>
+      <slot name="content"></slot>
+    </div>
   </div>
 </template>
 
@@ -20,6 +26,10 @@ div {
 
 h2 {
   color: red;
+}
+
+.title {
+  font-style: italic
 }
 </style>
 
