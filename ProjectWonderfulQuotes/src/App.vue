953317d1a4c4ec4c7ffdfda67eafb43032b96875
shihanxiong <shihanxiong@gmail.com>
2018-03-04 22:03:38 -0500

    update

--- ProjectWonderfulQuotes/src/App.vue
+++ ProjectWonderfulQuotes/src/App.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="container">
+      <NewQuote @QuoteAdded="newQuote"></NewQuote>
       <QuoteGrid :quotes="quotes"></QuoteGrid>
     </div>
 </template>
 
 <script>
 import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
 
 export default {
   data: function() {
@@ -14,8 +16,14 @@ export default {
       maxQuotes: 10
     };
   },
+  methods: {
+    newQuote(quote) {
+      this.quotes.push(quote);
+    }
+  },
   components: {
-    QuoteGrid: QuoteGrid
+    QuoteGrid: QuoteGrid,
+    NewQuote: NewQuote
   }
 };
 </script>
