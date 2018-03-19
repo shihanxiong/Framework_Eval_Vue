fd662e5944db7847e3cc14f0fc2638a33a290f8c
shihanxiong <shihanxiong@gmail.com>
2018-03-19 00:45:43 -0400

    update.

--- FormInput/src/App.vue
+++ FormInput/src/App.vue
@@ -84,6 +84,11 @@
                     </select>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <app-switch v-model="dataSwitch"></app-switch>
+                </div>
+            </div>
             <hr>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
@@ -111,7 +116,7 @@
                         </ul>
                         <p>Gender: {{ gender }}</p>
                         <p>Priority:</p>
-                        <p>Switched:</p>
+                        <p>Switched: {{ dataSwitch }}</p>
                     </div>
                 </div>
             </div>
@@ -120,6 +125,8 @@
 </template>
 
 <script>
+import Switch from "./Switch.vue";
+
 export default {
   data() {
     return {
@@ -132,8 +139,12 @@ export default {
       sendMail: [],
       gender: "Male",
       selectedPriority: "High",
-      priorities: ["High", "Medium", "Low"]
+      priorities: ["High", "Medium", "Low"],
+      dataSwitch: true,
     };
+  },
+  components: {
+    appSwitch: Switch
   }
 };
 </script>
